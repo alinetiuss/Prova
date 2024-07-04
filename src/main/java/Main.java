b7f6e2826f6f390496a7bf7552250bab72e52a26
AlineTiuss <169068304+alinetiuss@users.noreply.github.com>
2024-07-04 10:56:42 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+
+    System.out.println("Digite seu nome:");
+    String nome;
+    System.out.println("");
+
+    System.out.println("Digite seu sobrenome:");
+    String sobrenome;
+    System.out.println("");
+
+    System.out.println("Digite sua idade:");
+    String idade;
+    System.out.println("");
+  
+    System.out.println("Digite sua altura:");
+    String altura;
+    System.out.println("");
+
+    System.out.println("Digite seu sexo: 1-feminino 2-masculino:");
+    System.out.println("");
+
+    System.out.println("Olá fizemos uma breve análise das informações passadas e temos as seguintes informações a repassar.");
+    System.out.println("");
+
+    System.out.println("Seu peso ideal é:");
+    System.out.println("");
+
+    System.out.println("Obrigado por usar nosso aplicativo!");
+    
+
+  }
+}
